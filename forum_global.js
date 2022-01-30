b5e0d7f1a9c342295467bdb8af29f1f323f98062
srwieber <98571215+srwieber@users.noreply.github.com>
2022-01-29 20:17:25 -0800

    Update forum_global.js

--- forum_global.js
+++ forum_global.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 
+  $('.forumline tr:first-of-type td:first-of-type table div div').has('iframe').closest('.forumline').addClass('topad');
+
 // MFL league info. not in use. 
   var league_id = '38933';
   var year = 2021;
@@ -15,8 +17,8 @@ $( document ).ready(function() {
 // remove jump to
   $('.gensmall:contains("Jump to:")').remove();
 
-// This is the current user id for the forum
-//  console.log(_userdata["user_id"]);  
+  // This is the current user id for the forum
+  //  console.log(_userdata["user_id"]);  
   if ( _userdata["user_id"] == 1 ) {
     $('#page-footer').show();
   }
