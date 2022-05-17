ff17b25e07774b687883c29005364191c21956cd
srwieber <98571215+srwieber@users.noreply.github.com>
2022-05-16 18:39:33 -0700

    Update homepage_subforums.js

--- forum/homepage_subforums.js
+++ forum/homepage_subforums.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
 // remove new post icon legend
-  $('#content-container table').not('.forumline').find('span.gensmall:contains("No new posts")').closest('table').remove();
+//  $('#content-container table').not('.forumline').find('span.gensmall:contains("No new posts")').closest('table').remove();
   
 });
