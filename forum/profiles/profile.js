61534290365c148ed628c94a1967e925eca28902
srwieber <98571215+srwieber@users.noreply.github.com>
2024-02-01 21:02:48 -0800

    Create profile.js

---
+++ forum/profiles/profile.js
@@ -0,0 +1,276 @@
+/* Profile Images */
+  const profileAlaskaArsenic = [
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628108208511-77HXOHPP2LL85ZLTPXS3/wallpaperarsenic.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1618760296651-FQ3LSPB83JOIS1YI7T0R/2020bestnewadditionaward.jpg?format=1500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/661c438b-7555-4028-af5a-e6e6a93a6a55/2021waittilnextyear.png?format=1500w'
+  ];
+  const profileAtlantaAfricans = [
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1586721760045-T9UFMBN9PIMO7EOWPLUQ/teamafricans.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1587320209977-HHE65ZBOW5OAUL6R3SZT/fieldafricans.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/f24eed75-c6f2-4bbe-a992-186285bf4103/bannermvp2021.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628106161985-ZV7EXXEM3PMKLVEA0EOA/bannermvp2019.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628106954778-4K9K0WAOM9CR65IJDPBZ/wallpaperafricans.jpg?format=2500w'
+  ];
+  const profileAustraliaKillerKoalas = [
+    'https://i.servimg.com/u/f39/15/70/85/29/sigkil17.jpg',
+    'https://i48.servimg.com/u/f48/17/23/87/15/miniki10.png'
+  ];
+  const profileBuffaloBlizzard = [
+    'https://i.servimg.com/u/f39/15/70/85/29/sigbli20.jpg',
+    'https://i39.servimg.com/u/f39/15/70/85/29/minibl11.png'
+  ];
+  const profileCaliforniaNightmare = [
+    'https://i.servimg.com/u/f39/15/70/85/29/signig17.jpg',
+    'https://i48.servimg.com/u/f48/17/23/87/15/minini10.png'
+  ];
+  const profileCharmCityJustice = [
+    'https://i.servimg.com/u/f39/15/70/85/29/sigjus13.jpg',
+    'https://i48.servimg.com/u/f48/17/23/87/15/miniju10.png'
+  ];
+  const profileCoachellaTremors = [
+    'https://i.servimg.com/u/f39/15/70/85/29/sigtre14.jpg',
+    'https://i48.servimg.com/u/f48/17/23/87/15/minitr10.png'
+  ];
+  const profileColoradoCrusaders = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileCrystalLakeCounselors = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileCycloneCityFalcons = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileDallasSpurs = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileDecaturFreakshow = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileEastTennesseeBadgers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileEnglandDragons = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileGainesvilleGators = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileHaddonfieldSlashers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileHamiltonSteelCats = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileHatterasBayMadGoats = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileIndianaMayhem = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileKaneCountyGoonSquad = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileLasVegasDeGenerates = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileLatviaUnicorns = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileLittletonSilverBallers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileLondonLegion = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMaplewoodDoppelgangers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMichiganMungMen = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMinnesotaEternals = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMotorCityMalachiCrunch = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMrsEternals = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMrsRedknights = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileMuskegoMuskies = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileNevadaNightHawks = [
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628106868605-RND1B6WOEIBEHBS3A4IC/wallpapernighthawks.jpg?format=2500w',
+    'https://i.servimg.com/u/f39/15/70/85/29/signig19.jpg',
+    'https://i39.servimg.com/u/f39/15/70/85/29/minini10.png'
+  ];
+  const profilePacificPuffins = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profilePhiladelphiaPigskins = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileRiverbankRaiders = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileRiversideRottweilers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileRockyMountainOysters = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileSanFranciscoGolddiggers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileStellenboschSaints = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileTarponSpringsRedKnights = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileTenaflyVipers = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileThiefRiverUnforgiven = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileTitletownTyrants = [
+    'https://i39.servimg.com/u/f39/15/70/85/29/top_ba12.jpg',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628106510239-257T6OEWWIG95B38F2B0/wallpapertyrants.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1629228681281-VDOONE9S7NW6UHR91F9U/Trophy+Room.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1586721030182-GEVKZYMCJSOVUWNFDQIB/fieldtyrants.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628713125823-DG1JSAA2TQM2VAQGCYVP/tyrantshq.jpg?format=2500w'
+  ];
+  const profileToontownWarlocks = [
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628108279713-EDGN6P4PPV4V73UWS8N2/wallpaperwarlocks.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1618760103450-9OPIDCRYFA5FZEH43EQS/2020thelmaandlouiseaward.jpg?format=1500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/890cd65c-dbef-460a-a37a-a5e27f2d4d4a/2021bestpost.png?format=1000w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/d4b7f550-5419-47a4-b49f-72609b130bae/2021worsttradepartner.png?format=1000w'
+  ];
+  const profileVegasBeachTigerSharks = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileWhiskeyCreekGadabouts = [
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png',
+    'https://www45.myfantasyleague.com/fflnetdynamic2021/38933_avatar.png'
+  ];
+  const profileWindyCityBlitz = [
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1628108454029-HD4JC4BYKD4OWSNLV4BE/wallpaperblitz.jpg?format=2500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/e2013466-0db0-4266-9112-9cc610671d5f/2021+Minor+League+Champions.png?format=1500w',
+    'https://images.squarespace-cdn.com/content/v1/5dd3134b27cd1842f6fc6257/1590444326341-N9NQY87NMITGJU0HM9Q9/2019+Minor+League+Champions.png?format=1500w'
+  ];
+
+
+
+/* Profile page setup */
+$('h1.h_member').closest('.forumline').addClass('profile_table');
+$('.profile_table').next('table').addClass('profile_email');
+$('.profile_email').after('<table width="100%" id="profile_images"><tbody><tr><td class="row1"></td></tr></tbody></table>');
+
+let profileImages = $('#profile_images td');
+let currentTeam = $('h1.h_member strong').text();
+currentTeam = currentTeam.replace(/\s+/g, '');
+
+/* user online */
+let onlineText = $('.profile_table tr:nth-of-type(4) table tr:nth-of-type(6) td span.gen').text();
+if (onlineText === 'Online'){
+    $('.profile_table tr:nth-of-type(4) table tr:nth-of-type(6) td span.gen').addClass('user_online');
+}
+
+/* GPT solution */
+const teamProfiles = {
+  'AlaskaArsenic': profileAlaskaArsenic,
+  'AtlantaAfricans': profileAtlantaAfricans,
+  'AustraliaKillerKoalas': profileAustraliaKillerKoalas,
+  'BuffaloBlizzard': profileBuffaloBlizzard,
+  'CaliforniaNightmare': profileCaliforniaNightmare,
+  'CharmCityJustice': profileCharmCityJustice,
+  'CoachellaTremors': profileCoachellaTremors,
+  'ColoradoCrusaders': profileColoradoCrusaders,
+  'CrystalLakeCounselors': profileCrystalLakeCounselors,
+  'CycloneCityFalcons': profileCycloneCityFalcons,
+  'DallasSpurs': profileDallasSpurs,
+  'DecaturFreakshow': profileDecaturFreakshow,
+  'EastTennesseeBadgers': profileEastTennesseeBadgers,
+  'EnglandDragons': profileEnglandDragons,
+  'GainesvilleGators': profileGainesvilleGators,
+  'HaddonfieldSlashers': profileHaddonfieldSlashers,
+  'HamiltonSteelCats': profileHamiltonSteelCats,
+  'HatterasBayMadGoats': profileHatterasBayMadGoats,
+  'IndianaMayhem': profileIndianaMayhem,
+  'KaneCountyGoonSquad': profileKaneCountyGoonSquad,
+  'LasVegasDeGenerates': profileLasVegasDeGenerates,
+  'LatviaUnicorns': profileLatviaUnicorns,
+  'LittletonSilverBallers': profileLittletonSilverBallers,
+  'LondonLegion': profileLondonLegion,
+  'MaplewoodDoppelgangers': profileMaplewoodDoppelgangers,
+  'MichiganMungMen': profileMichiganMungMen,
+  'MinnesotaEternals': profileMinnesotaEternals,
+  'MotorCityMalachiCrunch': profileMotorCityMalachiCrunch,
+  'MrsEternals': profileMrsEternals,
+  'MrsRedknights': profileMrsRedknights,
+  'MuskegoMuskies': profileMuskegoMuskies,
+  'NevadaNightHawks': profileNevadaNightHawks,
+  'PacificPuffins': profilePacificPuffins,
+  'PhiladelphiaPigskins': profilePhiladelphiaPigskins,
+  'RiverbankRaiders': profileRiverbankRaiders,
+  'RiversideRottweilers': profileRiversideRottweilers,
+  'RockyMountainOysters': profileRockyMountainOysters,
+  'SanFranciscoGolddiggers': profileSanFranciscoGolddiggers,
+  'StellenboschSaints': profileStellenboschSaints,
+  'TarponSpringsRedKnights': profileTarponSpringsRedKnights,
+  'TenaflyVipers': profileTenaflyVipers,
+  'ThiefRiverUnforgiven': profileThiefRiverUnforgiven,
+  'TitletownTyrants': profileTitletownTyrants,
+  'ToontownWarlocks': profileToontownWarlocks,
+  'VegasBeachTigerSharks': profileVegasBeachTigerSharks,
+  'WhiskeyCreekGadabouts': profileWhiskeyCreekGadabouts,
+  'WindyCityBlitz': profileWindyCityBlitz,
+};
+
+const currentTeamProfile = teamProfiles[currentTeam];
+
+if (currentTeamProfile) {
+  currentTeamProfile.map((element, index) => {
+    $(profileImages).append('<img src="' + element + '" />');
+  });
+}
